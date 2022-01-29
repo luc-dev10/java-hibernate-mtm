d97ad6bbccb6b40c05a89b15a6ae937bd99865b8
luc-dev10 <luc.dev1008@gmail.com>
2022-01-29 14:12:11 -0500

    add course entity

---
+++ src/main/java/com/lucio/entity/Course.java
@@ -0,0 +1,60 @@
+package com.lucio.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> studentList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public void addStudent(Student student) {
+
+        if (studentList == null)
+            studentList = new ArrayList<>();
+
+        studentList.add(student);
+    }
+
+}
